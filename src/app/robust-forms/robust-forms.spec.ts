5c8ea6b0a46aed72b9f0b3d5a202fa3f7bc1eaa5
dudemelo <dudemelo@gmail.com>
2017-05-30 15:25:34 -0300

    Fixed robust-forms unit test

--- src/app/robust-forms/robust-forms.spec.ts
+++ src/app/robust-forms/robust-forms.spec.ts
@@ -1,10 +1,10 @@
 import {
     RobustForms,
     Group,
-    Text
+    Text,
 } from './';
 
-describe('RobustForms', () => {
+describe('RobustForms :: RobustForms', () => {
 
     it('should create a group', () => {
         const jsonGroups:Array<any> = [{
@@ -17,13 +17,19 @@ describe('RobustForms', () => {
 
     it('should create a group with a question', () => {
         const jsonGroups:Array<any> = [{
-            description: "A simple group with a question",
-            questions: [{ description: 'A simple question', fieldType: 'text' }]
+            description: 'A simple group with a question',
+            questions: [{
+                code: 'question-01',
+                description: 'A simple question',
+                fieldType: 'text',
+                validations: []
+            }]
         }];
-        expect(RobustForms.fromJson(jsonGroups))
-            .toEqual(new Group('A simple group with a question', [
-                new Text('P-01', 'A simple question', 'text', [], '', '')
-            ]));
+        expect(RobustForms.fromJson(jsonGroups)).toEqual([
+            new Group('A simple group with a question', [
+                new Text('question-01', 'A simple question', 'text', [])
+            ])
+        ]);
     });
 
 });
