8f7c1b0c91bce6dd9eaf732e8b6719ee4875021d
Fagner Lima <fagnerlima91@gmail.com>
2017-06-01 10:38:12 -0300

    Fixes tests for ReactiveFormsFactory

--- src/app/robust-forms/reactive-forms-factory.spec.ts
+++ src/app/robust-forms/reactive-forms-factory.spec.ts
@@ -1,16 +1,24 @@
 import { FormGroup } from '@angular/forms';
 
 import { ReactiveFormsFactory } from '.';
-import { Group } from './group/group';
-import { Question, Text } from './question';
+import { DataTable, Group, Question, Text } from '.';
 
 describe('RobustForms :: ReactiveFormsFactory', () => {
   it('should create a FormGroup from Groups', () => {
     const groups: Group[] = [
       new Group('group-01', 'Group 01', 'fieldset', []),
-      new Group('group-02', 'Group 02', 'datatable', [])
+      new Group('group-02', 'Group 02', 'fieldset', [])
     ];
     
+    expect(ReactiveFormsFactory.createFormGroupFromGroups(groups)).toEqual(jasmine.any(FormGroup));
+  });
+
+  it('should create a FormGroup from DataTables', () => {
+    const groups: Group[] = [
+      new DataTable('group-01', 'Group 01', 'datatable', [], 'consumo-agua', [], []),
+      new DataTable('group-02', 'Group 02', 'datatable', [], 'efluente', [], [])
+    ];
+
     expect(ReactiveFormsFactory.createFormGroupFromGroups(groups)).toEqual(jasmine.any(FormGroup));
   });
 
