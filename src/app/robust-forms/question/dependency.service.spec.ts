27d12c5936ca4c5bf9ea6c85dbf4ac67add61f59
Fagner Lima <contato@fagnerlima.pro.br>
2017-06-19 11:21:03 -0300

    Updates tests

--- src/app/robust-forms/question/dependency.service.spec.ts
+++ src/app/robust-forms/question/dependency.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed, async, inject } from '@angular/core/testing';
 
 import { DependencyService } from '.';
 
-describe('Service: Dependency', () => {
+describe('RobustForms :: Question :: DependencyService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [DependencyService]
