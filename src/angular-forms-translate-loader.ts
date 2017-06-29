e1c8ebd71aed4489a47268ce8a62b6321f2d8360
Fagner Lima <contato@fagnerlima.pro.br>
2017-06-29 09:47:24 -0300

    Adds visibility in method

--- src/angular-forms-translate-loader.ts
+++ src/angular-forms-translate-loader.ts
@@ -3,7 +3,7 @@ import { Observable } from 'rxjs/Observable';
 
 export class AngularFormsTranslateLoader implements TranslateLoader {
 
-  getTranslation(lang: string): Observable<any> {
+  public getTranslation(lang: string): Observable<any> {
     return Observable.of(require(`./assets/i18n/${lang}.json`));
   }
 }
