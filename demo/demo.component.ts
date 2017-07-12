8cd45c9f3898b50be21f5ff331c15fa8f82f7191
Fagner Lima <contato@fagnerlima.pro.br>
2017-07-12 18:03:44 -0300

    Updates demo and tests

--- demo/demo.component.ts
+++ demo/demo.component.ts
@@ -1,5 +1,6 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 
+import { DemoService } from './demo.service';
 import { AngularFormsComponent } from '../src';
 
 @Component({
@@ -13,140 +14,20 @@ import { AngularFormsComponent } from '../src';
       <button class='btn btn-primary' (click)='getAnswersGroups()'>getAnswersGroups()</button>
       <button class='btn btn-primary' (click)='getAnswers()'>getAnswers()</button>
     </div>
-  `
+  `,
+  providers: [DemoService]
 })
-export class DemoComponent {
+export class DemoComponent implements OnInit {
 
-  public customForm: any[] = [
-    {
-      'code': 'G-01',
-      'description': 'Fieldset Group',
-      'type': 'group',
-      'questions': [
-        {
-          'name': 'Q-0101',
-          'description': 'Check Question 0101',
-          'type': 'check',
-          'defaultOption': false,
-          'validations': [
-            { 'type': 'pattern', 'message': 'Required field.', 'value': 'true' }
-          ]
-        },
-        {
-          'name': 'Q-0102',
-          'description': 'Radio Question 0102',
-          'type': 'radio',
-          'options': ['Option 1', 'Option 2'],
-          'defaultOption': 'Option 1',
-          'validations': [
-            { 'type': 'required', 'message': 'Required field.' }
-          ]
-        },
-        {
-          'name': 'Q-0103',
-          'description': 'Select Question 0103',
-          'type': 'select',
-          'options': ['Option 1', 'Option 2'],
-          'placeholder': 'Select',
-          'validations': [
-            { 'type': 'required', 'message': 'Required field.' }
-          ]
-        },
-        {
-          'name': 'Q-0104',
-          'description': 'Text Question 0104',
-          'type': 'text',
-          'placeholder': 'Text Question 0104',
-          'validations': [
-            { 'type': 'required', 'message': 'Required field.' },
-            { 'type': 'minlength', 'message': 'Min Length: 3', 'value': 3 },
-            { 'type': 'maxlength', 'message': 'Max Length: 10', 'value': 10 }
-          ]
-        },
-        {
-          'name': 'Q-0105',
-          'description': 'Text Question 0105',
-          'type': 'text',
-          'placeholder': 'Text Question 0105',
-          'validations': [
-            { 'type': 'required', 'message': 'Required field.' },
-            { 'type': 'pattern', 'message': 'Format: 00000-000', 'value': '^\\d{5}-\\d{3}$' }
-          ],
-          'mask': '00000-000'
-        },
-        {
-          'name': 'Q-0106',
-          'description': 'TextArea Question 0106',
-          'type': 'textarea',
-          'placeholder': 'TextArea Question 0106',
-          'validations': [
-            { 'type': 'required', 'message': 'Required field.' }
-          ],
-          'dependencies': [
-            { 'code': 'Q-0103', 'criteria': 'equals', 'expectedAnswer': 'Option 1' }
-          ]
-        }
-      ]
-    },
-    {
-      'code': 'G-02',
-      'description': 'DataTable Group',
-      'type': 'datatable',
-      'validations': [
-        { 'type': 'required', 'message': 'Min number of registers: 1.' },
-        { 'type': 'minlength', 'message': 'Min number of registers: 1.', 'value': 1 },
-        { 'type': 'maxlength', 'message': 'Max number of registers: 3.', 'value': 3 }
-      ],
-      'questions': [
-        [
-          {
-            'name': 'Q-0201',
-            'description': 'Select Question 0201',
-            'type': 'select',
-            'validations': [
-              { 'type': 'required', 'message': 'Required field.' }
-            ],
-            'options': ['Option 1', 'Option 2', 'Option 3'],
-            'placeholder': 'Select'
-          },
-          {
-            'name': 'Q-0202',
-            'description': 'Text Question 0202',
-            'type': 'text',
-            'validations': [
-              { 'type': 'required', 'message': 'Required field.' }
-            ],
-            'placeholder': 'Text Question 0202'
-          }
-        ],
-        [
-          {
-            'name': 'Q-0201',
-            'description': 'Select Question 0201',
-            'type': 'select',
-            'validations': [
-              { 'type': 'required', 'message': 'Required field.' }
-            ],
-            'options': ['Option 1', 'Option 2', 'Option 3'],
-            'placeholder': 'Select',
-            'answer': 'Option 2'
-          },
-          {
-            'name': 'Q-0202',
-            'description': 'Text Question 0202',
-            'type': 'text',
-            'validations': [
-              { 'type': 'required', 'message': 'Required field.' }
-            ],
-            'placeholder': 'Text Question 0202',
-            'answer': 'Answer 0202'
-          }
-        ]
-      ]
-    }
-  ];
+  public customForm: any[];
 
   @ViewChild('angularForms') public angularForms: AngularFormsComponent;
+
+  public constructor(private demoService: DemoService) { }
+
+  public ngOnInit(): void {
+    this.customForm = this.demoService.getForm();
+  }
 
   public getForm(): void {
     console.log(this.angularForms.getForm());
--- test/angular-forms.component.spec.ts
+++ test/angular-forms.component.spec.ts
@@ -11,6 +11,7 @@ import { AngularFormsTranslateLoader } from '../src/angular-forms-translate-load
 import { DataTableModule } from '../src/data-table';
 import { ValidationMessageModule } from '../src/validation-message/validation-message.module';
 import { Select, Text, Dependency } from '../src/question';
+import { DemoService } from '../demo/demo.service';
 
 describe('AngularFormsComponent', () => {
   let component: AngularFormsComponent;
@@ -38,134 +39,7 @@ describe('AngularFormsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AngularFormsComponent);
     component = fixture.componentInstance;
-    component.groups = <any>[
-      {
-        'code': 'G-01',
-        'description': 'Fieldset Group',
-        'type': 'group',
-        'questions': [
-          {
-            'name': 'Q-0101',
-            'description': 'Check Question 0101',
-            'type': 'check',
-            'defaultOption': false,
-            'validations': [
-              { 'type': 'pattern', 'message': 'Required field.', 'value': 'true' }
-            ]
-          },
-          {
-            'name': 'Q-0102',
-            'description': 'Radio Question 0102',
-            'type': 'radio',
-            'options': ['Option 1', 'Option 2'],
-            'defaultOption': 'Option 1',
-            'validations': [
-              { 'type': 'required', 'message': 'Required field.' }
-            ]
-          },
-          {
-            'name': 'Q-0103',
-            'description': 'Select Question 0103',
-            'type': 'select',
-            'options': ['Option 1', 'Option 2'],
-            'placeholder': 'Select',
-            'validations': [
-              { 'type': 'required', 'message': 'Required field.' }
-            ]
-          },
-          {
-            'name': 'Q-0104',
-            'description': 'Text Question 0104',
-            'type': 'text',
-            'placeholder': 'Text Question 0104',
-            'validations': [
-              { 'type': 'required', 'message': 'Required field.' },
-              { 'type': 'minlength', 'message': 'Min Length: 3', 'value': 3 },
-              { 'type': 'maxlength', 'message': 'Max Length: 10', 'value': 10 }
-            ]
-          },
-          {
-            'name': 'Q-0105',
-            'description': 'Text Question 0105',
-            'type': 'text',
-            'placeholder': 'Text Question 0105',
-            'validations': [
-              { 'type': 'required', 'message': 'Required field.' },
-              { 'type': 'pattern', 'message': 'Format: 00000-000', 'value': '^\\d{5}-\\d{3}$' }
-            ],
-            'mask': '00000-000'
-          },
-          {
-            'name': 'Q-0106',
-            'description': 'TextArea Question 0106',
-            'type': 'textarea',
-            'placeholder': 'TextArea Question 0106',
-            'validations': [
-              { 'type': 'required', 'message': 'Required field.' }
-            ],
-            'dependencies': [
-              { 'code': 'Q-0103', 'criteria': 'equals', 'expectedAnswer': 'Option 1' }
-            ]
-          }
-        ]
-      },
-      {
-        'code': 'G-02',
-        'description': 'DataTable Group',
-        'type': 'datatable',
-        'validations': [
-          { 'type': 'required', 'message': 'Min number of registers: 1.' },
-          { 'type': 'minlength', 'message': 'Min number of registers: 1.', 'value': 1 },
-          { 'type': 'maxlength', 'message': 'Max number of registers: 3.', 'value': 3 }
-        ],
-        'questions': [
-          [
-            {
-              'name': 'Q-0201',
-              'description': 'Select Question 0201',
-              'type': 'select',
-              'validations': [
-                { 'type': 'required', 'message': 'Required field.' }
-              ],
-              'options': ['Option 1', 'Option 2', 'Option 3'],
-              'placeholder': 'Select'
-            },
-            {
-              'name': 'Q-0202',
-              'description': 'Text Question 0202',
-              'type': 'text',
-              'validations': [
-                { 'type': 'required', 'message': 'Required field.' }
-              ],
-              'placeholder': 'Text Question 0202'
-            }
-          ],
-          [
-            {
-              'name': 'Q-0201',
-              'description': 'Select Question 0201',
-              'type': 'select',
-              'validations': [
-                { 'type': 'required', 'message': 'Required field.' }
-              ],
-              'options': ['Option 1', 'Option 2', 'Option 3'],
-              'placeholder': 'Select',
-              'answer': 'Option 2'
-            },
-            {
-              'name': 'Q-0202',
-              'description': 'Text Question 0202',
-              'type': 'text',
-              'validations': [
-                { 'type': 'required', 'message': 'Required field.' }
-              ],
-              'placeholder': 'Text Question 0202',
-              'answer': 'Answer 0202'
-            }
-          ]
-        ]
-      }
-    ];
+    component.groups = (new DemoService()).getForm();
 
     fixture.detectChanges();
   });
@@ -217,8 +91,6 @@ describe('AngularFormsComponent', () => {
 
   it('should call the getAnswers method', () => {
     const answers: Object = component.getAnswers();
-    console.log('########################################################');
-    console.log(JSON.stringify(answers));
 
     assert.isOk(answers);
   });
