ecc84e16f4c06d7a220fc17ee320d808a46ef9ef
Fagner Lima <contato@fagnerlima.pro.br>
2017-07-19 10:12:37 -0300

    Fixes the verification of dependencies

--- src/question/dependency.service.ts
+++ src/question/dependency.service.ts
@@ -33,10 +33,10 @@ export class DependencyService {
 
   private executeOperation(answerDependency: string, dependency: Dependency): boolean {
     const operations: { [type: string]: boolean } = {
-      'equals': answerDependency.toString() === dependency.expectedAnswer.toString(),
+      'equals': (null !== answerDependency ? answerDependency.toString() : '') === dependency.expectedAnswer.toString(),
       'lessthan': parseFloat(answerDependency) < parseFloat(dependency.expectedAnswer),
       'greaterthan': parseFloat(answerDependency) > parseFloat(dependency.expectedAnswer),
-      'notequals': answerDependency.toString() !== dependency.expectedAnswer.toString()
+      'notequals': (null !== answerDependency ? answerDependency.toString() : '') !== dependency.expectedAnswer.toString()
     };
 
     return operations[dependency.criteria];
