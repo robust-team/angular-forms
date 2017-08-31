258ff92290c5d10d7fe6bc85552fe4edbe29b31e
Fagner Lima <contato@fagnerlima.pro.br>
2017-08-31 14:57:26 -0300

    Updates QuestionFactory

--- src/factory/question-factory.ts
+++ src/factory/question-factory.ts
@@ -1,17 +1,20 @@
-import { Question, Text, TextArea, Checkbox, Radio, Select } from '../question';
+import { Question, QuestionType, Text, TextArea, Checkbox, Radio, Select } from '../question';
 
 export class QuestionFactory {
 
-  private static types: any = {
-    'checkbox': Checkbox,
-    'radio': Radio,
-    'select': Select,
-    'text': Text,
-    'textarea': TextArea,
-  };
-
   public static createSimpleQuestion(question: Question<any>): Question<any> {
-    return QuestionFactory.types[question.type].fromJson(question);
+    switch (question.type) {
+      case QuestionType.CHECKBOX:
+        return Checkbox.fromJson(<Checkbox>question);
+      case QuestionType.RADIO:
+        return Radio.fromJson(<Radio>question);
+      case QuestionType.SELECT:
+        return Select.fromJson(<Select>question);
+      case QuestionType.TEXT:
+        return Text.fromJson(<Text>question);
+      case QuestionType.TEXTAREA:
+        return TextArea.fromJson(<TextArea>question);
+    }
   }
 
   public static createQuestionList(questionList: Question<any>[]): Question<any>[] {
