8aeadecd2e251048250d31b7737ce9b810cb97e7
Fagner Lima <contato@fagnerlima.pro.br>
2017-08-31 17:25:51 -0300

    Updates the tests

--- test/question/radio.spec.ts
+++ test/question/radio.spec.ts
@@ -3,6 +3,6 @@ import { Radio } from '../../src/question';
 
 describe('AngularForms :: Question :: Radio', () => {
   it('should be instantiable', () => {
-    assert.ok(new Radio('P-01', 'A simple question', [], '', [], false, [], ''));
+    assert.ok(new Radio('P-01', 'A simple question', [], 'Option 1', [], true, ['Option 1', 'Option 2'], 'Option 2'));
   });
 });
--- test/question/text.spec.ts
+++ test/question/text.spec.ts
@@ -3,6 +3,6 @@ import { Text } from '../../src/question';
 
 describe('AngularForms :: Question :: Text', () => {
   it('should be instantiable', () => {
-    assert.ok(new Text('P-01', 'A simple question', [], '', [], false, null, 'Answer'));
+    assert.ok(new Text('P-01', 'A simple question', [], '8.50', [], false, 'decimal(10,2)', 'Answer'));
   });
 });
