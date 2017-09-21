4bf9923890064dacfc9618b4fea0908d035e21cd
Fagner Lima <contato@fagnerlima.pro.br>
2017-09-21 10:25:47 -0300

    Updates tests

--- test/question/select.spec.ts
+++ test/question/select.spec.ts
@@ -4,6 +4,10 @@ import { Select } from '../../src/question';
 describe('AngularForms :: Question :: Select', () => {
   it('should be instantiable', () => {
     assert.ok(new Select('P-01', 'A simple question', [], null, [], false, ['Option 1', 'Option 2'], 'Option 2', 'Option 2', 'Select'));
+    assert.ok(new Select('P-01', 'A simple question', [], null, [], false, [
+      { value: 'op-1', description: 'Option 1' },
+      { value: 'op-2', description: 'Option 2' }
+    ], 'Option 2', 'Option 2', 'Select'));
     assert.ok(new Select('P-01', 'A simple question'));
   });
 });
