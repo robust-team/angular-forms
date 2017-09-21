9847932b5f056828dbffbac645367b9c25e82321
Fagner Lima <contato@fagnerlima.pro.br>
2017-09-21 11:36:28 -0300

    Removes Injectable decoration for static services

--- src/question/select.service.ts
+++ src/question/select.service.ts
@@ -1,9 +1,7 @@
-import { Injectable } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
 import { Select } from '.';
 
-@Injectable()
 export class SelectService {
 
   public static onChangeOption(htmlFormControl: HTMLInputElement, formControl: FormControl, question: Select): void {
