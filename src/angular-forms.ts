ed064abceab9a3b0a9e74e4cd936f9c4590b182a
Fagner Lima <contato@fagnerlima.pro.br>
2017-09-28 17:23:39 -0300

    Implements use of Promises into AngularForms and ReactiveFormsFactory

--- src/angular-forms.ts
+++ src/angular-forms.ts
@@ -5,21 +5,31 @@ import { QuestionFactory } from './factory';
 
 export class AngularForms {
 
-  public static fromJson(jsonGroups: Group[]): Group[] {
-    return jsonGroups.map((group: Fieldset | DataTable) => {
-      const groupBuilder: GroupBuilder<any> = GroupType.FIELDSET === group.type
-        ? new FieldsetBuilder(group.code, group.description)
-        : new DataTableBuilder(group.code, group.description, (<DataTable>group).validations);
+  public static async fromJson(jsonGroups: Group<any>[]): Promise<Group<any>[]> {
+    return new Promise<Group<any>[]>(async (resolve: (groups: Group<any>[]) => void, reject: (error: string) => void) => {
+      try {
+        const groups: Group<any>[] = [];
 
-      for (const question of group.questions) {
-        groupBuilder.addQuestion(
-          GroupType.FIELDSET === group.type
-            ? QuestionFactory.createQuestion(<Question<any>>question)
-            : QuestionFactory.createQuestionList(<Question<any>[]>question)
-        );
+        for await (const group of jsonGroups) {
+          const groupBuilder: GroupBuilder<any> = GroupType.FIELDSET === group.type
+            ? new FieldsetBuilder(group.code, group.description)
+            : new DataTableBuilder(group.code, group.description, (<DataTable>group).validations);
+
+          for await (const question of group.questions) {
+            groupBuilder.addQuestion(
+              GroupType.FIELDSET === group.type
+                ? QuestionFactory.createQuestion(<Question<any>>question)
+                : QuestionFactory.createQuestionList(<Question<any>[]>question)
+            );
+          }
+
+          groups.push(groupBuilder.build());
+        }
+
+        resolve(groups);
+      } catch (error) {
+        reject(error);
       }
-
-      return groupBuilder.build();
     });
   }
 }
