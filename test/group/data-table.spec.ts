992454a3c3a0a8a5aa634937551ed1914fa9d4e7
Fagner Lima <contato@fagnerlima.pro.br>
2017-09-28 18:34:30 -0300

    Updates tests

--- test/group/data-table.spec.ts
+++ test/group/data-table.spec.ts
@@ -1,8 +1,24 @@
 import { assert } from 'chai';
 import { DataTable } from '../../src/group';
+import { Text } from '../../src/question/text';
+import { Required } from '../../src/validation';
 
 describe('AngularForms :: Group :: DataTable', () => {
   it('should be instantiable', () => {
     assert.ok(new DataTable('G-01', 'A simple group', [[], []], []));
+  });
+
+  it('should getQuestionByName method', () => {
+    const dataTable: DataTable = new DataTable('G-01', 'A simple group', [[new Text('Q-01', 'A simple question')]]);
+
+    assert.isTrue(dataTable.getQuestionByName('Q-01') instanceof Text);
+  });
+
+  it('should isRequired method', () => {
+    const dataTable1: DataTable = new DataTable('G-01', 'A simple group', [], [new Required('Required')]);
+    const dataTable2: DataTable = new DataTable('G-01', 'A simple group', [], []);
+
+    assert.isTrue(dataTable1.isRequired());
+    assert.isFalse(dataTable2.isRequired());
   });
 });
