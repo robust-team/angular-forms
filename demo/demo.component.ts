f45ac6ec7bb92a4303252592790495bd6732d6f7
Fagner Lima <contato@fagnerlima.pro.br>
2017-09-28 17:33:44 -0300

    Updates DemoComponent

--- demo/demo.component.ts
+++ demo/demo.component.ts
@@ -1,14 +1,21 @@
-import { Component, ViewChild, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
 
 import { DemoService } from './demo.service';
 import { AngularFormsComponent } from '../src';
+import { Fieldset } from '../src/group/fieldset';
 
 @Component({
   selector: 'rb-demo-app',
   template: `
     <div class='container'>
       <h1>AngularForms</h1>
-      <rb-angular-forms #angularForms [groups]="customForm" [readOnly]="false" lang="pt-BR"></rb-angular-forms>
+
+      <div class="alert alert-danger" *ngIf="errorMessage">
+        <p>{{ errorMessage }}</p>
+      </div>
+
+      <rb-angular-forms #angularForms [groups]="customForm" [readOnly]="false" lang="pt-BR" (error)="onError($event)"></rb-angular-forms>
+
       <button class="btn btn-primary" (click)="angularForms.submit()">submit()</button>
       <button class="btn btn-primary" (click)="isPristine()">isPristine()</button>
       <button class="btn btn-primary" (click)="isDirty()">isDirty()</button>
@@ -21,16 +28,29 @@ import { AngularFormsComponent } from '../src';
   `,
   providers: [DemoService]
 })
-export class DemoComponent implements OnInit {
+export class DemoComponent implements OnInit, AfterViewInit {
 
   public customForm: any[];
 
   @ViewChild('angularForms') public angularForms: AngularFormsComponent;
 
-  public constructor(private demoService: DemoService) { }
+  private _errorMessage: string;
+
+  public constructor(
+    private demoService: DemoService,
+    private changeDetectorRef: ChangeDetectorRef
+  ) { }
 
   public ngOnInit(): void {
     this.customForm = this.demoService.getForm();
+  }
+
+  public ngAfterViewInit(): void {
+    this.changeDetectorRef.detectChanges();
+  }
+
+  public onError(error: Error): void {
+    this._errorMessage = 'Error';
   }
 
   public getForm(): void {
@@ -55,5 +75,9 @@ export class DemoComponent implements OnInit {
 
   public getAnswers(): void {
     console.log(this.angularForms.getAnswers());
+  }
+
+  public get errorMessage(): string {
+    return this._errorMessage;
   }
 }
